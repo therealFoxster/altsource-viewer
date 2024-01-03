129c5c70726d1b4206b5d8838ed1932c98778a98
Foxster <huythefox@gmail.com>
2024-01-02 18:38:35 -0800

    Update URL regex to support URLs with parentheses

--- common/modules/constants.js
+++ common/modules/constants.js
@@ -11,7 +11,7 @@ export const urlSearchParams = new URLSearchParams(window.location.search);
 export const sourceURL = urlSearchParams.get('source')?.replaceAll("+", "%2B");
 
 // https://stackoverflow.com/a/8943487
-export const urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+export const urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;()]*[-A-Z0-9+&@#\/%=~_|)])/ig;
 
 export const sources = [
     "https://therealfoxster.github.io/altsource/apps.json",
