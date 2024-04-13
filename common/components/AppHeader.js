dd85b98cead9aaafa3a0e6de2eed137396d2b230
Foxster <huythefox@gmail.com>
2024-04-13 16:25:11 -0700

    Fix app header padding & margin

--- common/components/AppHeader.js
+++ common/components/AppHeader.js
@@ -11,7 +11,7 @@ import { sourceURL } from "../modules/constants.js";
 export const AppHeader = (app, x = ".") => app ? `
 <div class="app-header-container">
 <a href="${x}/app/?source=${sourceURL}&id=${app.bundleIdentifier}" class="app-header-link">
-    <div class="item">
+    <div class="app-header-inner-container">
         <div class="app-header">
             <div class="content">
                 <img id="app-icon" src="${app.iconURL}" alt="">
